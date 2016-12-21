34ca3ccbe9f8e2518562da9256a53c92c0af0b49
Pat Cavit <github@patcavit.com>
2016-12-21 00:07:35 -0800

    lodash.ismatchwith wrapper for easier comparison

---
+++ src/match.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var match = require("lodash.ismatchwith");
+
+// Simple wrapper around ismatchwith to allow for function keys in the filter object
+module.exports = (target, filter) =>
+    match(target, filter, (val, test) => {
+        // Support functions for comparisons
+        if(typeof val !== "object" && typeof test === "function") {
+            return test(val);
+        }
+    });
