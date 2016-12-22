ffeb73e0aaae46095faeaac1610aa066587fc24c
Pat Cavit <github@patcavit.com>
2016-12-21 21:56:34 -0800

    Re-enable mixed children tests
    THEY JUST WORKED OMG

--- test/children-mixed.test.js
+++ test/children-mixed.test.js
@@ -4,25 +4,25 @@ var assert = require("assert"),
 
     code = require("./lib/code");
 
-describe.skip("mithril-objectify", function() {
+describe("mithril-objectify", function() {
     describe("Children", function() {
         describe("Mixed Children", function() {
             it("should support mixed array and literal children", function() {
                 assert.equal(
                     code(`m("div", [ 1 ], 2)`),
-                    `m.vnode("div", [ 1 ], 2)`
+                    `m.vnode("div",undefined,undefined,m.vnode.normalizeChildren([[1],2]),undefined,undefined);`
                 );
             });
             
             it("should support multiple arrays of children", function() {
                 assert.equal(
                     code(`m("div", [ 1, 2, 3 ], [ 4, 5, 6, 7 ])`),
-                    `m.vnode("div", [ 1, 2, 3 ], [ 4, 5, 6, 7 ])`
+                    `m.vnode("div",undefined,undefined,m.vnode.normalizeChildren([[1,2,3],[4,5,6,7]]),undefined,undefined);`
                 );
                 
                 assert.equal(
                     code(`m("div", [ 1 ], [ 2 ], [ 3 ])`),
-                    `m.vnode("div", [ 1 ], [ 2 ], [ 3 ])`
+                    `m.vnode("div",undefined,undefined,m.vnode.normalizeChildren([[1],[2],[3]]),undefined,undefined);`
                 );
             });
         });
