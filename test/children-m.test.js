5b483d4a7cf571a0aa99c6bda7a707ae065a984b
Pat Cavit <github@patcavit.com>
2016-12-23 23:40:08 -0800

    test: easier-to-read test inputs/outputs

--- test/children-m.test.js
+++ test/children-m.test.js
@@ -8,22 +8,22 @@ describe("Children", function() {
     describe("Nested m()", function() {
         it("should support nested m() invocations", function() {
             assert.equal(
-                code(`m("0", m("div"))`),
-                `m.vnode("0",undefined,undefined,[m.vnode("div",undefined,undefined,[],undefined,undefined)],undefined,undefined);`
+                code(`m("0", m("1"))`),
+                `m.vnode("0",undefined,undefined,[m.vnode("1",undefined,undefined,[],undefined,undefined)],undefined,undefined);`
             );
         });
         
         it("should support an array with an m() invocation", function() {
             assert.equal(
-                code(`m("0", [ m("div") ])`),
-                `m.vnode("0",undefined,undefined,[m.vnode("div",undefined,undefined,[],undefined,undefined)],undefined,undefined);`
+                code(`m("0", [ m("1") ])`),
+                `m.vnode("0",undefined,undefined,[m.vnode("1",undefined,undefined,[],undefined,undefined)],undefined,undefined);`
             );
         });
         
         it("should support an array with m() invocations", function() {
             assert.equal(
-                code(`m("0", [ m("p"), m("i") ])`),
-                `m.vnode("0",undefined,undefined,[m.vnode("p",undefined,undefined,[],undefined,undefined),m.vnode("i",undefined,undefined,[],undefined,undefined)],undefined,undefined);`
+                code(`m("0", [ m("1"), m("2") ])`),
+                `m.vnode("0",undefined,undefined,[m.vnode("1",undefined,undefined,[],undefined,undefined),m.vnode("2",undefined,undefined,[],undefined,undefined)],undefined,undefined);`
             );
         });
 
@@ -36,29 +36,29 @@ describe("Children", function() {
         
         it("should support multiple nested m() invocations", function() {
             assert.equal(
-                code(`m("0", m("div"), m("i"), m("span"))`),
-                `m.vnode("0",undefined,undefined,[m.vnode("div",undefined,undefined,[],undefined,undefined),m.vnode("i",undefined,undefined,[],undefined,undefined),m.vnode("span",undefined,undefined,[],undefined,undefined)],undefined,undefined);`
+                code(`m("0", m("1"), m("2"), m("3"))`),
+                `m.vnode("0",undefined,undefined,[m.vnode("1",undefined,undefined,[],undefined,undefined),m.vnode("2",undefined,undefined,[],undefined,undefined),m.vnode("3",undefined,undefined,[],undefined,undefined)],undefined,undefined);`
             );
         });
         
         it("should support attrs + nested m() invocations", function() {
             assert.equal(
-                code(`m("0", { title : "bar" }, m("div"))`),
-                `m.vnode("0",undefined,{title:"bar"},[m.vnode("div",undefined,undefined,[],undefined,undefined)],undefined,undefined);`
+                code(`m("0", { title : "bar" }, m("1"))`),
+                `m.vnode("0",undefined,{title:"bar"},[m.vnode("1",undefined,undefined,[],undefined,undefined)],undefined,undefined);`
             );
         });
         
         it("should supported attrs + arrays of nested m() invocations", function() {
             assert.equal(
-                code(`m("0", { title : "bar" }, [ m("div") ])`),
-                `m.vnode("0",undefined,{title:"bar"},[m.vnode("div",undefined,undefined,[],undefined,undefined)],undefined,undefined);`
+                code(`m("0", { title : "bar" }, [ m("1") ])`),
+                `m.vnode("0",undefined,{title:"bar"},[m.vnode("1",undefined,undefined,[],undefined,undefined)],undefined,undefined);`
             );
         });
         
         it("should support attrs + multiple nested m() invocations", function() {
             assert.equal(
-                code(`m("0", { title : "bar" }, m("div"), m("i"), m("span"))`),
-                `m.vnode("0",undefined,{title:"bar"},[m.vnode("div",undefined,undefined,[],undefined,undefined),m.vnode("i",undefined,undefined,[],undefined,undefined),m.vnode("span",undefined,undefined,[],undefined,undefined)],undefined,undefined);`
+                code(`m("0", { title : "bar" }, m("1"), m("2"), m("3"))`),
+                `m.vnode("0",undefined,{title:"bar"},[m.vnode("1",undefined,undefined,[],undefined,undefined),m.vnode("2",undefined,undefined,[],undefined,undefined),m.vnode("3",undefined,undefined,[],undefined,undefined)],undefined,undefined);`
             );
         });
     });
