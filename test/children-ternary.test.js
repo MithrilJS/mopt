ee5e3be85aed0c4e0802ee579a6035553e845a94
Pat Cavit <github@patcavit.com>
2017-06-04 00:22:42 -0700

    refactor: ternary children tests in jest

--- test/children-ternary.test.js
+++ test/children-ternary.test.js
@@ -1,44 +1,42 @@
 "use strict";
 
-var assert = require("assert"),
+var code = require("./lib/code");
 
-    code = require("./lib/code");
+describe("Children", () => {
+    describe("Ternary expressions", () => {
+        it("should convert when all entries are literals (strings)", () =>
+            expect(
+                code(`m("0", foo ? "1" : "2")`)
+            )
+            .toMatchSnapshot()
+        );
 
-describe("Children", function() {
-    describe("Ternary expressions", function() {
-        it("should convert when all entries are literals (strings)", function() {
-            assert.equal(
-                code(`m("0", foo ? "1" : "2")`),
-                `m.vnode("0",undefined,undefined,undefined,foo?"1":"2",undefined);`
-            );
-        });
+        it("should convert when all entries are literals (string : number)", () =>
+            expect(
+                code(`m("0", foo ? "1" : 2)`)
+            )
+            .toMatchSnapshot()
+        );
 
-        it("should convert when all entries are literals (string : number)", function() {
-            assert.equal(
-                code(`m("0", foo ? "1" : 2)`),
-                `m.vnode("0",undefined,undefined,undefined,foo?"1":2,undefined);`
-            );
-        });
-
-        it("should convert nested ternaries when all entries are literals (strings)", function() {
-            assert.equal(
-                code(`m("0", foo ? "1" : bar ? "2" : "3")`),
-                `m.vnode("0",undefined,undefined,undefined,foo?"1":bar?"2":"3",undefined);`
-            );
-        });
+        it("should convert nested ternaries when all entries are literals (strings)", () =>
+            expect(
+                code(`m("0", foo ? "1" : bar ? "2" : "3")`)
+            )
+            .toMatchSnapshot()
+        );
         
-        it("should not convert when entries are not literals", function() {
+        it("should not convert when entries are not literals", () => {
             // Can't convert this, dunno what `bar` is
-            assert.equal(
-                code(`m("0", foo ? bar : "baz")`),
-                `m("0",foo?bar:"baz");`
-            );
+            expect(
+                code(`m("0", foo ? bar : "baz")`)
+            )
+            .toMatchSnapshot();
             
             // Not a text child, so can't safely optimize
-            assert.equal(
-                code(`m("0", foo ? { class : options.class } : null)`),
-                `m("0",foo?{class:options.class}:null);`
-            );
+            expect(
+                code(`m("0", foo ? { class : options.class } : null)`)
+            )
+            .toMatchSnapshot();
         });
     });
 });
