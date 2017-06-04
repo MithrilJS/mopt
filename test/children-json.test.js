b5a74a573eec41ab6e94bf6daf63e44ba3b7c28a
Pat Cavit <github@patcavit.com>
2017-06-04 00:16:04 -0700

    refactor: children json tests to jest

--- test/children-json.test.js
+++ test/children-json.test.js
@@ -1,23 +1,21 @@
 "use strict";
 
-var assert = require("assert"),
+var code = require("./lib/code");
 
-    code   = require("./lib/code");
-
-describe("Children", function() {
-    describe("JSON.prototype children", function() {
-        it("should know that JSON.stringify is safe", function() {
-            assert.equal(
-                code(`m("div", JSON.stringify({}))`),
-                `m.vnode("div",undefined,undefined,undefined,JSON.stringify({}),undefined);`
-            );
-        });
+describe("Children", () => {
+    describe("JSON.prototype children", () => {
+        it("should know that JSON.stringify is safe", () =>
+            expect(
+                code(`m("div", JSON.stringify({}))`)
+            )
+            .toMatchSnapshot()
+        );
         
-        it("shouldn't transform JSON.parse since it may not be safe", function() {
-            assert.equal(
-                code(`m("div", JSON.parse({}))`),
-                `m("div",JSON.parse({}));`
-            );
-        });
+        it("shouldn't transform JSON.parse since it may not be safe", () =>
+            expect(
+                code(`m("div", JSON.parse({}))`)
+            )
+            .toMatchSnapshot()
+        );
     });
 });
