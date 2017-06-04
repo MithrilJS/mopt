800a69c02d9a174ed7ec9e590799847d70a600da
Pat Cavit <github@patcavit.com>
2017-06-04 00:15:02 -0700

    refactor: expression children test to jest

--- test/children-expressions.test.js
+++ test/children-expressions.test.js
@@ -1,51 +1,49 @@
 "use strict";
 
-var assert = require("assert"),
-    
-    code = require("./lib/code");
+var code = require("./lib/code");
 
-describe("Children", function() {
-    describe("Expression children", function() {
-        it("should support expressions with literal values (strings)", function() {
-            assert.equal(
-                code(`m("0", "1" + "2")`),
-                `m.vnode("0",undefined,undefined,undefined,"1"+"2",undefined);`
-            );
-        });
+describe("Children", () => {
+    describe("Expression children", () => {
+        it("should support expressions with literal values (strings)", () =>
+            expect(
+                code(`m("0", "1" + "2")`)
+            )
+            .toMatchSnapshot()
+        );
 
-        it("should support expressions with literal values (numbers)", function() {
-            assert.equal(
-                code(`m("0", 1 + 2)`),
-                `m.vnode("0",undefined,undefined,undefined,1+2,undefined);`
-            );
-        });
+        it("should support expressions with literal values (numbers)", () =>
+            expect(
+                code(`m("0", 1 + 2)`)
+            )
+            .toMatchSnapshot()
+        );
 
-        it("should support expressions with literal values (strings + numbers)", function() {
-            assert.equal(
-                code(`m("0", 1 + "2")`),
-                `m.vnode("0",undefined,undefined,undefined,1+"2",undefined);`
-            );
-        });
+        it("should support expressions with literal values (strings + numbers)", () =>
+            expect(
+                code(`m("0", 1 + "2")`)
+            )
+            .toMatchSnapshot()
+        );
 
-        it("should convert expressions containing more than 2 values", function() {
-            assert.equal(
-                code(`m("0", "1" + "2" + "3")`),
-                `m.vnode("0",undefined,undefined,undefined,"1"+"2"+"3",undefined);`
-            );
-        });
+        it("should convert expressions containing more than 2 values", () =>
+            expect(
+                code(`m("0", "1" + "2" + "3")`)
+            )
+            .toMatchSnapshot()
+        );
 
-        it("should convert expressions with operators other than +", function() {
-            assert.equal(
-                code(`m("0", 1 - 2)`),
-                `m.vnode("0",undefined,undefined,undefined,1-2,undefined);`
-            );
-        });
+        it("should convert expressions with operators other than +", () =>
+            expect(
+                code(`m("0", 1 - 2)`)
+            )
+            .toMatchSnapshot()
+        );
         
-        it("should not convert non-literal values", function() {
-            assert.equal(
-                code(`m("0", a + b)`),
-                `m("0",a+b);`
-            );
-        });
+        it("should not convert non-literal values", () =>
+            expect(
+                code(`m("0", a + b)`)
+            )
+            .toMatchSnapshot()
+        );
     });
 });
