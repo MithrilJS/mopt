a137dbf8be15b587771a3d48f6e09871df1edcb3
Pat Cavit <github@patcavit.com>
2017-06-04 00:20:49 -0700

    refactor: mixed children tests in jest

--- test/children-mixed.test.js
+++ test/children-mixed.test.js
@@ -1,23 +1,21 @@
 "use strict";
 
-var assert = require("assert"),
+var code = require("./lib/code");
 
-    code = require("./lib/code");
-
-describe("Children", function() {
-    describe("Mixed Children", function() {
-        it("should support mixed array and literal children", function() {
-            assert.equal(
-                code(`m("0", [ 1 ], 2)`),
-                `m.vnode("0",undefined,undefined,[m.vnode("[",undefined,undefined,[m.vnode("#",undefined,undefined,1,undefined,undefined)],undefined,undefined),m.vnode("#",undefined,undefined,2,undefined,undefined)],undefined,undefined);`
-            );
-        });
+describe("Children", () => {
+    describe("Mixed Children", () => {
+        it("should support mixed array and literal children", () =>
+            expect(
+                code(`m("0", [ 1 ], 2)`)
+            )
+            .toMatchSnapshot()
+        );
         
-        it("should support multiple arrays of children", function() {
-            assert.equal(
-                code(`m("0", [ 1 ], [ 2 ], [ 3 ])`),
-                `m.vnode("0",undefined,undefined,[m.vnode("[",undefined,undefined,[m.vnode("#",undefined,undefined,1,undefined,undefined)],undefined,undefined),m.vnode("[",undefined,undefined,[m.vnode("#",undefined,undefined,2,undefined,undefined)],undefined,undefined),m.vnode("[",undefined,undefined,[m.vnode("#",undefined,undefined,3,undefined,undefined)],undefined,undefined)],undefined,undefined);`
-            );
-        });
+        it("should support multiple arrays of children", () =>
+            expect(
+                code(`m("0", [ 1 ], [ 2 ], [ 3 ])`)
+            )
+            .toMatchSnapshot()
+        );
     });
 });
