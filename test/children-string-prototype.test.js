2c9b63dbc3145e10a3dc016a9306edd82ba835fc
Pat Cavit <github@patcavit.com>
2017-06-04 00:21:38 -0700

    refactor: string.prototype children tests in jest

--- test/children-string-prototype.test.js
+++ test/children-string-prototype.test.js
@@ -1,23 +1,21 @@
 "use strict";
 
-var assert = require("assert"),
-    
-    code = require("./lib/code");
+var code = require("./lib/code");
 
-describe("Children", function() {
-    describe("String.prototype method children", function() {
-        it("should support String.prototype methods using dot notation", function() {
-            assert.equal(
-                code(`m("div", "fooga".replace("f", "g"))`),
-                `m.vnode("div",undefined,undefined,undefined,"fooga".replace("f","g"),undefined);`
-            );
-        });
+describe("Children", () => {
+    describe("String.prototype method children", () => {
+        it("should support String.prototype methods using dot notation", () =>
+            expect(
+                code(`m("div", "fooga".replace("f", "g"))`)
+            )
+            .toMatchSnapshot()
+        );
         
-        it("should support String.prototype methods using array notation", function() {
-            assert.equal(
-                code(`m("div", "fooga"["replace"]("f", "g"))`),
-                `m.vnode("div",undefined,undefined,undefined,"fooga"["replace"]("f","g"),undefined);`
-            );
-        });
+        it("should support String.prototype methods using array notation", () =>
+            expect(
+                code(`m("div", "fooga"["replace"]("f", "g"))`)
+            )
+            .toMatchSnapshot()
+        );
     });
 });
