9feecc981a14d88321055231dc261d381dedd0d3
Pat Cavit <github@patcavit.com>
2017-06-04 00:18:12 -0700

    refactor: m children tests to jest

--- test/children-m.test.js
+++ test/children-m.test.js
@@ -1,65 +1,63 @@
 "use strict";
 
-var assert = require("assert"),
+var code = require("./lib/code");
 
-    code = require("./lib/code");
+describe("Children", () => {
+    describe("Nested m()", () => {
+        it("should support nested m() invocations", () =>
+            expect(
+                code(`m("0", m("1"))`)
+            )
+            .toMatchSnapshot()
+        );
+        
+        it("should support an array with an m() invocation", () =>
+            expect(
+                code(`m("0", [ m("1") ])`)
+            )
+            .toMatchSnapshot()
+        );
+        
+        it("should support an array with m() invocations", () =>
+            expect(
+                code(`m("0", [ m("1"), m("2") ])`)
+            )
+            .toMatchSnapshot()
+        );
 
-describe("Children", function() {
-    describe("Nested m()", function() {
-        it("should support nested m() invocations", function() {
-            assert.equal(
-                code(`m("0", m("1"))`),
-                `m.vnode("0",undefined,undefined,[m.vnode("1",undefined,undefined,[],undefined,undefined)],undefined,undefined);`
-            );
-        });
+        it("should support arrays of nested m() invocations & plain nodes", () =>
+            expect(
+                code(`m("one", [ m("two"), "three" ])`)
+            )
+            .toMatchSnapshot()
+        );
         
-        it("should support an array with an m() invocation", function() {
-            assert.equal(
-                code(`m("0", [ m("1") ])`),
-                `m.vnode("0",undefined,undefined,[m.vnode("1",undefined,undefined,[],undefined,undefined)],undefined,undefined);`
-            );
-        });
+        it("should support multiple nested m() invocations", () =>
+            expect(
+                code(`m("0", m("1"), m("2"), m("3"))`)
+            )
+            .toMatchSnapshot()
+        );
         
-        it("should support an array with m() invocations", function() {
-            assert.equal(
-                code(`m("0", [ m("1"), m("2") ])`),
-                `m.vnode("0",undefined,undefined,[m.vnode("1",undefined,undefined,[],undefined,undefined),m.vnode("2",undefined,undefined,[],undefined,undefined)],undefined,undefined);`
-            );
-        });
-
-        it("should support arrays of nested m() invocations & plain nodes", function() {
-            assert.equal(
-                code(`m("one", [ m("two"), "three" ])`),
-                `m.vnode("one",undefined,undefined,[m.vnode("two",undefined,undefined,[],undefined,undefined),m.vnode("#",undefined,undefined,"three",undefined,undefined)],undefined,undefined);`
-            );
-        });
+        it("should support attrs + nested m() invocations", () =>
+            expect(
+                code(`m("0", { title : "bar" }, m("1"))`)
+            )
+            .toMatchSnapshot()
+        );
         
-        it("should support multiple nested m() invocations", function() {
-            assert.equal(
-                code(`m("0", m("1"), m("2"), m("3"))`),
-                `m.vnode("0",undefined,undefined,[m.vnode("1",undefined,undefined,[],undefined,undefined),m.vnode("2",undefined,undefined,[],undefined,undefined),m.vnode("3",undefined,undefined,[],undefined,undefined)],undefined,undefined);`
-            );
-        });
+        it("should supported attrs + arrays of nested m() invocations", () =>
+            expect(
+                code(`m("0", { title : "bar" }, [ m("1") ])`)
+            )
+            .toMatchSnapshot()
+        );
         
-        it("should support attrs + nested m() invocations", function() {
-            assert.equal(
-                code(`m("0", { title : "bar" }, m("1"))`),
-                `m.vnode("0",undefined,{title:"bar"},[m.vnode("1",undefined,undefined,[],undefined,undefined)],undefined,undefined);`
-            );
-        });
-        
-        it("should supported attrs + arrays of nested m() invocations", function() {
-            assert.equal(
-                code(`m("0", { title : "bar" }, [ m("1") ])`),
-                `m.vnode("0",undefined,{title:"bar"},[m.vnode("1",undefined,undefined,[],undefined,undefined)],undefined,undefined);`
-            );
-        });
-        
-        it("should support attrs + multiple nested m() invocations", function() {
-            assert.equal(
-                code(`m("0", { title : "bar" }, m("1"), m("2"), m("3"))`),
-                `m.vnode("0",undefined,{title:"bar"},[m.vnode("1",undefined,undefined,[],undefined,undefined),m.vnode("2",undefined,undefined,[],undefined,undefined),m.vnode("3",undefined,undefined,[],undefined,undefined)],undefined,undefined);`
-            );
-        });
+        it("should support attrs + multiple nested m() invocations", () =>
+            expect(
+                code(`m("0", { title : "bar" }, m("1"), m("2"), m("3"))`)
+            )
+            .toMatchSnapshot()
+        );
     });
 });
