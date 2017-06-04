4f7836cbd91ad60c94a5a4c347fb738c38fcaccb
Pat Cavit <github@patcavit.com>
2017-06-04 00:25:22 -0700

    refactor: svg tests in jest

--- test/svg.test.js
+++ test/svg.test.js
@@ -1,21 +1,19 @@
 "use strict";
 
-var assert = require("assert"),
+var code = require("./lib/code");
 
-    code = require("./lib/code");
-
-describe("SVG", function() {
-    it("should support svg selectors", function() {
-        assert.equal(
-            code(`m("svg")`),
-            `m.vnode("svg",undefined,undefined,[],undefined,undefined);`
-        );
-    });
+describe("SVG", () => {
+    it("should support svg selectors", () =>
+        expect(
+            code(`m("svg")`)
+        )
+        .toMatchSnapshot()
+    );
     
-    it("should support nested svg selectors", function() {
-        assert.equal(
-            code(`m("svg", m("g"))`),
-            `m.vnode("svg",undefined,undefined,[m.vnode("g",undefined,undefined,[],undefined,undefined)],undefined,undefined);`
-        );
-    });
+    it("should support nested svg selectors", () =>
+        expect(
+            code(`m("svg", m("g"))`)
+        )
+        .toMatchSnapshot()
+    );
 });
